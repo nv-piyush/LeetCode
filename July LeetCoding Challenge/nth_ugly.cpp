54b175ad45a374856e23ea57141497c413d16659
Piyush Jadhav <40065258+nv-piyush@users.noreply.github.com>
2020-07-05 23:54:53 +0530

    DP approach
    Time Complexity: O(n)
    Auxiliary Space: O(n)

---
+++ July LeetCoding Challenge/nth_ugly.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        static vector<int> result(1,1);
+        static int i =0;
+        static int j =0;
+        static int k = 0;
+        while(result.size()<n)
+        {
+            result.push_back(min(result[i]*2, min(result[j]*3, result[k]*5)));
+            if(result.back() == result[i]*2)
+                i++;
+            if(result.back() == result[j]*3)
+                j++;
+            if(result.back() == result[k]*5)
+                k++;
+        }
+        return result[n-1];
+    }
+};
